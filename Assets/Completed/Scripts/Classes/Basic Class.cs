5d4632eec43c65c10994c3837d787e58bba5cdf3
Panix <Panix>
2016-02-10 15:08:57 +0900

    Added simple base class; Started turn based strategy script

---
+++ Assets/Completed/Scripts/Classes/Basic Class.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicClass
+{
+    private string className;
+    private string classDescription;
+
+    //Stats
+    private int str;
+    private int agi;
+    private int def;
+
+    public string ClassName
+    {
+        get { return className; }
+        set { className = value; }
+    }
+
+    public string ClassDescription
+    {
+        get { return classDescription; }
+        set { classDescription = value; }
+    }
+
+    public int Str
+    {
+        get { return str; }
+        set { str = value; }
+    }
+
+    public int Agi
+    {
+        get { return agi; }
+        set { agi = value; }
+    }
+
+    public int Def
+    {
+        get { return def; }
+        set { def = value; }
+    }
+}
---
+++ Assets/Completed/Scripts/Turn Based Combat/TurnBasedStateMachine.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnBasedStateMachine : MonoBehaviour
+{
+
+    public enum States
+    {
+        START,
+        PLAYERCHOICE,
+        ENEMYCHOICE,
+        LOSE,
+        WIN
+    }
+
+    private States currentState;
+
+	// Use this for initialization
+	void Start () 
+    {
+        currentState = States.START;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Debug.Log(currentState);
+	    switch(currentState)
+        {
+            case (States.START):
+                // Setup battle function
+                break;
+            case (States.PLAYERCHOICE):
+                break;
+            case (States.ENEMYCHOICE):
+                break;
+            case (States.LOSE):
+                break;
+            case (States.WIN):
+                break;
+        }
+	}
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("NEXT STATE"))
+        {
+            if(currentState == States.START)
+            {
+                currentState = States.PLAYERCHOICE;
+            }
+            else if (currentState == States.PLAYERCHOICE)
+            {
+                currentState = States.ENEMYCHOICE;
+            }
+            else if (currentState == States.ENEMYCHOICE)
+            {
+                currentState = States.LOSE;
+            }
+            else if (currentState == States.LOSE)
+            {
+                currentState = States.WIN;
+            }
+            else if (currentState == States.WIN)
+            {
+                currentState = States.START;
+            }
+        }
+    }
+}
